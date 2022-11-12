e2353bfcb14579c5d501e62e8f2424fd2c59756f
Bjarte Aune Olsen <bjarte.aune.olsen@nho.no>
2022-11-12 15:25:24 +0100

    Upgraded the OpenAI package to get createImage method

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,3 +1,25 @@
+// import { Configuration, OpenAIApi } from "openai";
+
+// const openai = new OpenAIApi(
+//   new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+//   })
+// );
+
+// export default async function (req, res) {
+
+//   console.log(req.body.animal);
+
+//   const response = await openai.createImage({
+//     prompt: "a white siamese cat",
+//     n: 1,
+//     size: "1024x1024",
+//   });
+
+//   res.status(200).json({ result: response.data.data[0].url });
+
+// }
+
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
@@ -6,19 +28,28 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
+
+  const text = await openai.createCompletion({
     model: "text-davinci-002",
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+
+  const image = await openai.createImage({
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+  });
+
+  console.log(image.data.data[0].url);
+
+  res.status(200).json({ result: text.data.choices[0].text });
 }
 
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
-
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
